4748a66f062e8b6e2e4110ee38c3a22d85398d7c
Alex Forey <me@alexforey.com>
2013-04-02 15:59:06 +0100

    Updated assets

--- background.js
+++ background.js
@@ -3,14 +3,35 @@ chrome.contextMenus.create({
 	"title": "Save to Markpond",
 	"contexts": ["page", "selection", "link"],
 	"onclick" : function(e) {
-		var url = e.pageUrl;
-		var markpondURL = "https://markpond.com/core/bookmarklets/chrome-context?";
-          
-        if (e.selectionText) { markpondURL += "title=" + encodeURIComponent(e.selectionText) + "&"; }
-          
-        if (e.linkUrl) { url = e.linkUrl; }
-          
-        markpondURL += "url=" + encodeURIComponent(url);
-        $('#load').load(markpondURL);
+
+        var url = e.pageUrl;
+
+        var endpoint = "http://localhost:3000/bookmarklets/tiny"
+
+        if (e.selectionText) { excerpt = encodeURIComponent(e.selectionText); }
+        if (e.linkUrl) { url = encodeURIComponent(e.linkUrl); }
+
+        $.ajax({
+        	type: "POST",
+        	url: endpoint,
+        	data: {
+        		bookmark_url: url,
+        		source: "Chrome Extension",
+        		excerpt: excerpt
+        	},
+        	cache: false,
+        	complete: function(jqXHR, textStatus) {
+        		switch (jqXHR.status) {
+        			case 200:
+        				alert("Saved Successfully");
+        				break;
+        			case 401:
+        				alert("Please log in to Markpond first");
+        				break;
+        			default:
+        				alert("Something went wrong. Please try agian.")
+        		}
+        	}
+        });
     }
 });
